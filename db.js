0ce0205e7f422c8feeb5386b129049d7a35fba99
Koupy <nanook.tech@proton.me>
2024-02-27 11:06:34 +0100

    Update db.js

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ const { Sequelize } = require("sequelize");
 const sequelize = new Sequelize(
   process.env.DB_NAME, // Database name
   process.env.DB_USER, // User
-  process.env.DB_PSSWD, // Password
+  process.env.DB_PASSWORD, // Password
   {
     host: process.env.DB_HOST, // Host
     dialect: "postgres",
